3423dd63dae25730c3515d0911331d684e0d4d87
Ogbodo Uchenna (Osinachi) <ogbodouchenna1425@gmail.com>
2022-02-03 18:04:10 +0100

    cleaned my component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { AppBar } from '@material-ui/core';
 import './App.css';
 import Header from './components/header/Header';
-import NavAppBar from './components/header/HeaderChildren.jsx/NavAppBar';
 
 const App =()=> {
   return (
     <div className="App">
         <Header/>
-        {/* <NavAppBar/> */}
     </div>
   );
 }
